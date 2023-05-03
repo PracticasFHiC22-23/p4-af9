fb3518d4d371d74546391327103a9c515db072fd
Junjie_Li <jfjunjieli@gmail.com>
2023-05-04 00:36:02 +0200

    feat: header

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    }
+  }
+}
+</script>
+
+<template>
+  <v-card>
+    <v-layout>
+      <v-app-bar color="primary" prominent flat="true">
+        <v-spacer></v-spacer>
+
+        <v-btn variant="plain" class="text-none" :ripple="false"> Examenes </v-btn>
+
+        <v-btn variant="plain" class="text-none" :ripple="false"> Libres </v-btn>
+
+        <v-btn variant="plain" class="text-none" :ripple="false"> Sales </v-btn>
+
+        <v-btn icon="mdi-dots-grid"></v-btn>
+
+        <v-btn variant="text" icon="mdi-account"></v-btn>
+      </v-app-bar>
+    </v-layout>
+  </v-card>
+</template>
