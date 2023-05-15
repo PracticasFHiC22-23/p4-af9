1f448e1c5b3fc291d20b60f6fe92351b32bb792b
Junjie_Li <jfjunjieli@gmail.com>
2023-05-15 16:24:21 +0200

    feat: index page

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -14,21 +14,29 @@ export default {
 </script>
 
 <template>
-  <v-card>
-    <v-layout>
-      <v-app-bar color="primary" prominent flat="true">
-        <v-spacer></v-spacer>
+  <header>
+    <v-card>
+      <v-layout>
+        <v-app-bar color="primary" prominent flat="true">
+          <v-spacer></v-spacer>
 
-        <v-btn variant="plain" class="text-none" :ripple="false"> Examenes </v-btn>
+          <v-btn variant="plain" class="text-none" :ripple="false"> Examenes </v-btn>
 
-        <v-btn variant="plain" class="text-none" :ripple="false"> Libres </v-btn>
+          <v-btn variant="plain" class="text-none" :ripple="false"> Libres </v-btn>
 
-        <v-btn variant="plain" class="text-none" :ripple="false"> Sales </v-btn>
+          <v-btn variant="plain" class="text-none" :ripple="false"> Sales </v-btn>
 
-        <v-btn icon="mdi-dots-grid"></v-btn>
+          <v-btn icon="mdi-dots-grid"></v-btn>
 
-        <v-btn variant="text" icon="mdi-account"></v-btn>
-      </v-app-bar>
-    </v-layout>
-  </v-card>
+          <v-btn variant="text" icon="mdi-account"></v-btn>
+        </v-app-bar>
+      </v-layout>
+    </v-card>
+  </header>
 </template>
+
+<style scoped>
+header {
+  height: 10vh;
+}
+</style>
