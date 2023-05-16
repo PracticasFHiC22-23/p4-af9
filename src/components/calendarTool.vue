2748e8a60a1cf51218822d863d5b0e226efb1176
Junjie_Li <jfjunjieli@gmail.com>
2023-05-16 03:37:47 +0200

    fix: calendari

--- src/components/calendarTool.vue
+++ src/components/calendarTool.vue
@@ -6,9 +6,11 @@
       :loading="loading"
       :class-names="classNames"
       :meetings-days="meetingsDays"
+      v-model="meeting"
       @next-date="nextDate"
       @previous-date="previousDate"
     />
+    <p>meeting Selected: {{ meeting ? meeting : 'No Meeting selected' }}</p>
   </div>
 </template>
 
@@ -16,12 +18,12 @@
 import { defineComponent, onMounted, ref, computed } from 'vue'
 
 import VueMeetingSelector from '../../node_modules/vue-meeting-selector/dist/vue-meeting-selector.mjs'
-// Function used to generate slots, use your own function
 import slotsGenerator from '../../node_modules/vue-meeting-selector/src/helpers/slotsGenerator'
 
 import MeetingsDay from '../../node_modules/vue-meeting-selector/src/interfaces/MeetingsDay.interface'
 import MeetingSlot from '../../node_modules/vue-meeting-selector/src/interfaces/MeetingSlot.interface'
-import Time from '../../node_modules/vue-meeting-selector/src/interfaces/Time.interface'
+import CalendarOptions from '../../node_modules/vue-meeting-selector/src/interfaces/CalendarOptions.interface'
+
 
 export default defineComponent({
   name: 'SimpleExample',
@@ -30,25 +32,23 @@ export default defineComponent({
   },
   setup() {
     const date = ref(new Date())
-    const meetingsDays = ref<MeetingsDay[]>([])
+    const meetingsDays = ref<MeetingsDay | null>(null)
     const meeting = ref<MeetingSlot | null>(null)
     const loading = ref(true)
 
     const nbDaysToDisplay = computed(() => 5)
 
-    // because of line-height, font-type you might need to change top value
     const classNames = computed(() => ({
       tabLoading: 'loading-div'
     }))
 
-    // juste set async the gettings of meeting to display loading
     const slotsGeneratorAsync = (
-      d: Date, // date
-      n: number, // nbDaysToDisplay
-      start: Time,
-      end: Time,
-      timesBetween: number
-    ): Promise<MeetingsDay[]> =>
+      d, // date
+      n,
+      start,
+      end,
+      timesBetween
+    ) =>
       new Promise((resolve) => {
         setTimeout(() => {
           resolve(slotsGenerator(d, n, start, end, timesBetween))
@@ -57,34 +57,34 @@ export default defineComponent({
 
     const nextDate = async () => {
       loading.value = true
-      const start: Time = {
-        hours: 8,
+      const start = {
+        hours: 9,
         minutes: 0
       }
-      const end: Time = {
-        hours: 16,
+      const end = {
+        hours: 20,
         minutes: 0
       }
       const dateCopy = new Date(date.value)
       const newDate = new Date(dateCopy.setDate(dateCopy.getDate() + 7))
       date.value = newDate
-      meetingsDays.value = await slotsGeneratorAsync(newDate, nbDaysToDisplay.value, start, end, 30)
+      meetingsDays.value = await slotsGeneratorAsync(newDate, nbDaysToDisplay.value, start, end, 60)
       loading.value = false
     }
 
     const previousDate = async () => {
       loading.value = true
-      const start: Time = {
-        hours: 8,
+      const start = {
+        hours: 9,
         minutes: 0
       }
-      const end: Time = {
+      const end = {
         hours: 20,
         minutes: 0
       }
       const dateCopy = new Date(date.value)
       dateCopy.setDate(dateCopy.getDate() - 7)
-      const formatingDate = (dateToFormat: Date): String => {
+      const formatingDate = (dateToFormat) => {
         const d = new Date(dateToFormat)
         const day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate()
         const month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1
@@ -94,16 +94,16 @@ export default defineComponent({
       const newDate =
         formatingDate(new Date()) >= formatingDate(dateCopy) ? new Date() : new Date(dateCopy)
       date.value = newDate
-      meetingsDays.value = await slotsGeneratorAsync(newDate, nbDaysToDisplay.value, start, end, 30)
+      meetingsDays.value = await slotsGeneratorAsync(newDate, nbDaysToDisplay.value, start, end, 60)
       loading.value = false
     }
 
     onMounted(async () => {
-      const start: Time = {
-        hours: 8,
+      const start = {
+        hours: 9,
         minutes: 0
       }
-      const end: Time = {
+      const end = {
         hours: 20,
         minutes: 0
       }
@@ -123,6 +123,7 @@ export default defineComponent({
       loading,
       nbDaysToDisplay,
       classNames,
+      meeting,
       nextDate,
       previousDate
     }
@@ -133,7 +134,6 @@ export default defineComponent({
 <style scoped lang="scss">
 .simple-example {
   &__meeting-selector {
-    max-width: 700px;
     width: 700px;
   }
 }
